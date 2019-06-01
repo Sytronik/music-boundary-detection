aab3be758ab9e793d08592503dbf85fb85c8c8fc
Sytronik <sytronik210@gmail.com>
2019-06-01 13:02:10 +0900

    complete SALAMIDataset and Normalization comment test_loader

--- data_manager.py
+++ data_manager.py
@@ -22,16 +22,15 @@ from utils import DataPerDevice
 class Normalization:
     @staticmethod
     def _sum(a: ndarray) -> ndarray:
-        return a.sum(axis=1, keepdims=True)
+        return a.sum(axis=-1, keepdims=True)
 
     @staticmethod
     def _sq_dev(a: ndarray, mean_a: ndarray) -> ndarray:
-        return ((a - mean_a)**2).sum(axis=1, keepdims=True)
+        return ((a - mean_a)**2).sum(axis=-1, keepdims=True)
 
     @staticmethod
     def _load_data(fname: Union[str, Path], queue: mp.Queue) -> None:
-        # TODO: load x
-        x = None
+        x = np.load(fname)
         queue.put(x)
 
     @staticmethod
@@ -62,12 +61,12 @@ class Normalization:
         # Calculate summation & size (parallel)
         list_fn = (np.size, cls._sum)
         pool_loader = mp.Pool(2)
-        pool_calc = mp.Pool(max(mp.cpu_count() - 4, 2))
+        pool_calc = mp.Pool(min(mp.cpu_count()-2, 6))
         with mp.Manager() as manager:
             queue_data = manager.Queue()
             pool_loader.starmap_async(cls._load_data,
                                       [(f, queue_data) for f in all_files])
-            result: List[mp.pool.AsyncResult] = [None] * len(all_files)
+            result = [None] * len(all_files)
             for idx in tqdm(range(len(all_files)), desc='mean', dynamic_ncols=True):
                 data = queue_data.get()
                 result[idx] = pool_calc.apply_async(
@@ -89,7 +88,7 @@ class Normalization:
             queue_data = manager.Queue()
             pool_loader.starmap_async(cls._load_data,
                                       [(f, queue_data) for f in all_files])
-            result: List[mp.pool.AsyncResult] = [None] * len(all_files)
+            result = [None] * len(all_files)
             for idx in tqdm(range(len(all_files)), desc='std', dynamic_ncols=True):
                 data = queue_data.get()
                 result[idx] = pool_calc.apply_async(
@@ -131,13 +130,13 @@ class Normalization:
         return a
 
 
-# Class based on PyTorch Dataset
 class SALAMIDataset(Dataset):
     def __init__(self, kind_data: str, hparams, normalization=None):
-        self._PATH = Path(hparams.dataset_path)
-        self.all_files = [f for f in self._PATH.glob('*') if 'normconst' not in f.name]
+        self._PATH = hparams.path_feature[kind_data]
+        self.all_files = [f for f in self._PATH.glob('*.npy')]
         self.all_files = sorted(self.all_files)
-        self.all_files = np.random.permutation(self.all_files)
+        # self.all_files = list(np.random.permutation(self.all_files).tolist())
+        self.all_y = dict(**np.load(self._PATH / f'{hparams.segmap}_maps.npz'))
 
         if kind_data == 'train':
             f_normconst = self._PATH / 'normconst.npz'
@@ -150,21 +149,23 @@ class SALAMIDataset(Dataset):
             assert normalization
             self.normalization = normalization
 
-    def __getitem__(self, index: int) -> Tuple[Tensor, Tensor, int]:
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, int]:
         """
 
-        :param index:
+        :param idx:
         :return: (x, y, len_x)
             x: tensor with size (F, T)
             y: tensor with size (T,)
             len_x: an integer T
         """
 
-        # TODO: open feature file, normalize and return
-        x = None
-        y = None
-        len_x = None  # x.shape[1]
-        return (x, y, len_x)
+        # open feature file and return
+        fname = self.all_files[idx]
+        x = torch.tensor(np.load(fname), dtype=torch.float32)
+        y = torch.tensor(self.all_y[fname.name.split('_')[0]], dtype=torch.int64)
+        len_x = x.shape[2]
+        y = y[:len_x]
+        return x, y, len_x
 
     def __len__(self):
         return len(self.all_files)
@@ -172,16 +173,16 @@ class SALAMIDataset(Dataset):
     @staticmethod
     def pad_collate(batch):
         # stack samples with padding
-        batch_x = [item[0].permute(1, 0) for item in batch]
-        batch_x = pad_sequence(batch_x, batch_first=True)  # B, T, F
-        batch_x = batch_x.permute(0, 2, 1)  # B, F, T
-        batch_x = batch_x.unsqueeze(1)  # B, 1, F, T
+        batch_x = [item[0].permute(2, 0, 1) for item in batch]
+        batch_x = pad_sequence(batch_x, batch_first=True)  # B, T, C, F
+        batch_x = batch_x.permute(0, 2, 3, 1)  # B, C, F, T
 
         batch_y = [item[1] for item in batch]
         batch_y = pad_sequence(batch_y, batch_first=True)  # B, T
 
-        len_x = torch.tensor([item[2] for item in batch], dtype=torch.float32)
-        len_x = len_x.unsqueeze(-1)  # B, 1
+        # len_x = torch.tensor([item[2] for item in batch], dtype=torch.float32)
+        # len_x = len_x.unsqueeze(-1)  # B, 1
+        len_x = [item[2] for item in batch]
 
         return batch_x, batch_y, len_x
 
@@ -193,7 +194,7 @@ class SALAMIDataset(Dataset):
         and only one element can have the value of -1 which means that
         it's automaticall set to the value so that the sum of the elements is 1
 
-        :type dataset: Dataset
+        :type dataset: SALAMIDataset
         :type ratio: Sequence[float]
 
         :rtype: Sequence[Dataset]
@@ -213,10 +214,10 @@ class SALAMIDataset(Dataset):
         idx_data = np.cumsum(np.insert(ratio, 0, 0) * len(dataset.all_files),
                              dtype=int)
         result = [copy(dataset) for _ in range(n_split)]
-        # all_f_per = np.random.permutation(a.all_files)
+        all_f_per = np.random.permutation(dataset.all_files).tolist()
 
         for ii in range(n_split):
-            result[ii].all_files = dataset.all_files[idx_data[ii]:idx_data[ii + 1]]
+            result[ii].all_files = all_f_per[idx_data[ii]:idx_data[ii + 1]]
 
         return result
 
@@ -225,7 +226,7 @@ class SALAMIDataset(Dataset):
 def get_dataloader(hparams):
     salami = SALAMIDataset('train', hparams)
     train_set, valid_set = SALAMIDataset.split(salami, (hparams.train_ratio, -1))
-    test_set = SALAMIDataset('test', hparams, salami.normalization)
+    # test_set = SALAMIDataset('test', hparams, salami.normalization)
 
     train_loader = DataLoader(train_set,
                               batch_size=hparams.batch_size,
@@ -241,12 +242,13 @@ def get_dataloader(hparams):
                               num_workers=hparams.num_workers,
                               pin_memory=True,
                               collate_fn=SALAMIDataset.pad_collate)
-    test_loader = DataLoader(test_set,
-                             batch_size=hparams.batch_size,
-                             shuffle=False,
-                             drop_last=False,
-                             num_workers=hparams.num_workers,
-                             pin_memory=True,
-                             collate_fn=SALAMIDataset.pad_collate)
+    # test_loader = DataLoader(test_set,
+    #                          batch_size=hparams.batch_size,
+    #                          shuffle=False,
+    #                          drop_last=False,
+    #                          num_workers=hparams.num_workers,
+    #                          pin_memory=True,
+    #                          collate_fn=SALAMIDataset.pad_collate)
 
-    return train_loader, valid_loader, test_loader
+    # return train_loader, valid_loader, test_loader
+    return train_loader, valid_loader, None
