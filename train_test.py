e7b6d1119d1518280d0cbde25c259b8e0e01ff72
hjlee9605 <hjlee9605@gmail.com>
2019-06-04 21:26:35 +0900

    add peak_picking as function to train_test.py delete peak_picking.py

--- train_test.py
+++ train_test.py
@@ -115,8 +115,30 @@ class Runner(object):
 
     @staticmethod
     def predict(out):
-        # TODO: return prediction of class
-        pass
+
+        N_6s = round(6 * hparams.sample_rate / hparams.hop_size) - 1
+        N_12s = round(12 * hparams.sample_rate / hparams.hop_size) - 1
+
+        candid_val = []
+        candid_idx = []
+        for i in range(len(out)):
+            if i < N_6s:
+                if out[i] >= np.amax(out[:i + N_6s + 1]):
+                    candid_val.append(out[i])
+                    candid_idx.append(i)
+            else:
+                if out[i] >= np.amax(out[i - N_6s:i + N_6s + 1]):
+                    candid_val.append(out[i])
+                    candid_idx.append(i)
+
+        threshold = []
+        for j in candid_idx:
+            if j < N_12s:
+                threshold.append(out[j] - np.mean(out[0:j + N_6s + 1]))
+            else:
+                threshold.append(out[j] - np.mean(out[j - N_12s:j + N_6s + 1]))
+
+        boundary_idx = np.where(out > np.amax(threshold))
 
     @staticmethod
     def eval(prediction, truth):
