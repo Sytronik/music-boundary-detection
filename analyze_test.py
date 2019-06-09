af9664b52c1c2a275a284d36041be2b20f9028ed
chungks603 <chungks159@gmail.com>
2019-06-09 22:09:00 +0900

    figure label, font, value in text, save figure not show

--- analyze_test.py
+++ analyze_test.py
@@ -10,7 +10,7 @@ from hparams import hparams
 
 # test_eval: precision, recall, fscore
 path_test = Path(hparams.logdir, 'test')
-path_metadata = hparams.path_dataset['test'] /'metadata/metadata.csv'
+path_metadata = hparams.path_dataset['test'] / 'metadata/metadata.csv'
 
 # Take the genres of each song in id order
 with path_metadata.open('r', encoding='utf-8') as f:
@@ -56,14 +56,21 @@ genre_err = np.array([min_err, max_err])
 genre_total = np.array([genre_mean, genre_min, genre_max])
 genre_total = genre_total.transpose((2, 1, 0))
 
+xs = np.arange(len(genre_result))
+plt.rcParams['font.family'] = 'Arial'
 fig, ax = plt.subplots()
-ax.errorbar(np.arange(len(genre_result)), genre_mean[:, 2], yerr=genre_err[:, :, 2],
+ax.errorbar(xs, genre_mean[:, 2], yerr=genre_err[:, :, 2],
             linestyle='', marker='o')
+ax.set_xticks(xs)
 ax.set_xticklabels(genre_result)
-plt.show()
+ax.set_ylabel('F1-score')
+for x, y in zip(xs, genre_mean[:, 2]):
+    plt.text(x + 0.1, y, f'{y:.3f}')
+ax.set_xlim(xs[0] - 0.8, xs[-1] + 0.8)
+fig.savefig(path_test / 'test.png', dpi=300)
 
 # genre(precision, recall, f1), total -> CSV
-with open(path_test/'test.csv', 'w', encoding='utf-8', newline='') as f:
+with open(path_test / 'test.csv', 'w', encoding='utf-8', newline='') as f:
     writer = csv.writer(f, delimiter=',')
     writer.writerow(['GENRE',
                      'PRECISION mean', 'PRECISION min', 'PRECISION max',
