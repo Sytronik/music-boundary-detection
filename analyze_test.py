dd9663332c51e1f747b3f5c64b8aa96522e4db92
Sytronik <sytronik210@gmail.com>
2019-06-10 15:15:20 +0900

    can accept argument (test epoch and logdir) and refactor

--- analyze_test.py
+++ analyze_test.py
@@ -1,6 +1,7 @@
 import csv
 from collections import defaultdict
 from pathlib import Path
+from argparse import ArgumentParser
 
 import matplotlib.pyplot as plt
 import mir_eval
@@ -8,70 +9,83 @@ import numpy as np
 
 from hparams import hparams
 
-# test_eval: precision, recall, fscore
-path_test = Path(hparams.logdir, 'test')
-path_metadata = hparams.path_dataset['test'] /'metadata/metadata.csv'
+
+def main():
+    # test_eval: precision, recall, fscore
+    path_test = Path(hparams.logdir, f'test_{test_epoch}')
+    path_metadata = hparams.path_dataset['test'] / 'metadata/metadata.csv'
 
 # Take the genres of each song in id order
-with path_metadata.open('r', encoding='utf-8') as f:
-    read = csv.reader(f)
-    id_genre = dict()  # k: id, v: genre
-    for idx, line in enumerate(read):
-        if idx == 0:
-            continue
-        id_ = line[0]
-        if (path_test / f'{id_}_pred.npy').exists():
-            id_genre[id_] = line[4]
+    with path_metadata.open('r', encoding='utf-8') as f:
+        read = csv.reader(f)
+        id_genre = dict()  # k: id, v: genre
+        i_col_genre = 3
+        for idx, line in enumerate(read):
+            if idx == 0:
+                i_col_genre = line.index('GENRE')
+                continue
+            id_ = line[0]
+            if (path_test / f'{id_}_pred.npy').exists():
+                id_genre[id_] = line[i_col_genre]
 
-test_result = dict()  # k: id, v: float
-for id_ in id_genre.keys():
-    item_truth = np.load(path_test / f'{id_}_truth.npy')
-    item_pred = np.load(path_test / f'{id_}_pred.npy')
-    test_result[id_] = mir_eval.segment.detection(item_truth, item_pred, trim=True)
+    test_result = dict()  # k: id, v: float
+    for id_ in id_genre.keys():
+        item_truth = np.load(path_test / f'{id_}_truth.npy')
+        item_pred = np.load(path_test / f'{id_}_pred.npy')
+        test_result[id_] = mir_eval.segment.detection(item_truth, item_pred, trim=True)
 
-total = list(test_result.values())
-total_mean = np.mean(total, axis=0)
-total_max = np.max(total, axis=0)
-total_min = np.min(total, axis=0)
-total_result = np.array([total_mean, total_min, total_max])
-total_result = total_result.transpose((1, 0))
+    total = list(test_result.values())  # (N, 3) list
+    total_mean = np.mean(total, axis=0)  # (3,)
+    total_min = np.min(total, axis=0)  # (3,)
+    total_max = np.max(total, axis=0)  # (3,)
+    total_result = np.array([total_mean, total_min, total_max])  # (3, 3)
+    total_result = total_result.T  # (3: metrics, 3: mean-min-max)
 
-genre_result = defaultdict(list)  # k: genre, v: list
-for id_, g in id_genre.items():
-    genre_result[g].append(test_result[id_])
+    genre_result = defaultdict(list)  # k: genre, v: list
+    for id_, g in id_genre.items():
+        genre_result[g].append(test_result[id_])
 
-all_genre = list(genre_result.values())
-genre_mean = []
-genre_max = []
-genre_min = []
-for list_result in genre_result.values():
-    genre_mean.append(np.mean(list_result, axis=0))
-    genre_max.append(np.max(list_result, axis=0))
-    genre_min.append(np.min(list_result, axis=0))
+    all_genre = list(genre_result.keys())
+    num_genres = len(genre_result)
+    genre_mean = np.zeros((num_genres, 3))
+    genre_max = np.zeros((num_genres, 3))
+    genre_min = np.zeros((num_genres, 3))
+    for idx, g in enumerate(all_genre):
+        genre_mean[idx] = np.mean(genre_result[g], axis=0)
+        genre_max[idx] = np.max(genre_result[g], axis=0)
+        genre_min[idx] = np.min(genre_result[g], axis=0)
 
-genre_mean = np.array(genre_mean)
-min_err = genre_mean - np.array(genre_min)
-max_err = np.array(genre_max) - genre_mean
-genre_err = np.array([min_err, max_err])
-genre_total = np.array([genre_mean, genre_min, genre_max])
-genre_total = genre_total.transpose((2, 1, 0))
+    # genre_mean = np.array(genre_mean)
+    min_err = genre_mean - genre_min
+    max_err = genre_max - genre_mean
+    genre_err = np.stack((min_err, max_err), axis=0)  # 2, num_genres, 3
+    genre_total = np.array([genre_mean, genre_min, genre_max])  # 3, genre, 3
+    genre_total = genre_total.transpose((2, 1, 0))  # 3: metrics, num_genres, 3: mean-min-max
 
-fig, ax = plt.subplots()
-ax.errorbar(np.arange(len(genre_result)), genre_mean[:, 2], yerr=genre_err[:, :, 2],
-            linestyle='', marker='o')
-ax.set_xticklabels(genre_result)
-plt.show()
+    fig, ax = plt.subplots()
+    ax.errorbar(np.arange(len(genre_result)), genre_mean[:, 2], yerr=genre_err[:, :, 2],
+                linestyle='', marker='o')
+    ax.set_xticklabels(genre_result)
+    plt.show()
 
-# genre(precision, recall, f1), total -> CSV
-with open(path_test/'test.csv', 'w', encoding='utf-8', newline='') as f:
-    writer = csv.writer(f, delimiter=',')
-    writer.writerow(['GENRE',
-                     'PRECISION mean', 'PRECISION min', 'PRECISION max',
-                     'RECALL mean', 'RECALL min', 'RECALL max',
-                     'F1-score mean', 'F1-score min', 'F1-score max'])
+    # genre(precision, recall, f1), total -> CSV
+    with open(path_test / 'test.csv', 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(['GENRE',
+                         'PRECISION mean', 'PRECISION min', 'PRECISION max',
+                         'RECALL mean', 'RECALL min', 'RECALL max',
+                         'F1-score mean', 'F1-score min', 'F1-score max'])
 
-    for g, precision, recall, fscore in \
-            zip(genre_result.keys(), genre_total[0], genre_total[1], genre_total[2]):
-        writer.writerow([g, *precision.tolist(), *recall.tolist(), *fscore.tolist()])
+        for idx, g in enumerate(all_genre):
+            writer.writerow([g, *genre_total[:, idx, :].flatten().tolist()])
 
-    writer.writerow(['TOTAL', *total_result[0].tolist(), *total_result[1].tolist(), *total_result[2].tolist()])
+        writer.writerow(['TOTAL', *total_result.flatten().tolist()])
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('epoch', type=int)
+
+    args = hparams.parse_argument(parser)
+    test_epoch = args.epoch
+    main()
