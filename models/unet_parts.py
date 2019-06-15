8032f5a67a4545d8fcaa64f7dc3596c328fff927
Sytronik <sytronik210@gmail.com>
2019-06-15 12:32:15 +0900

    Final model

--- models/unet_parts.py
+++ models/unet_parts.py
@@ -5,11 +5,8 @@ sub-parts of the U-Net model
 from typing import Tuple
 
 import numpy as np
-import torch
-from torch import nn, Tensor
 import torch.nn.functional as F
-
-from .cbam import CBAM
+from torch import nn, Tensor
 
 
 def force_size_same(a: Tensor, b: Tensor) -> Tuple[Tensor, Tensor]:
@@ -46,57 +43,14 @@ class ConvBNAct(nn.Module):
         return self.cba(x)
 
 
-# Residual block
-class ResidualBlock(nn.Module):
-    def __init__(self, in_ch: int, out_ch: int, act_fn: nn.Module,
-                 last_act_fn=True, use_cbam=False):
-        super().__init__()
-        self.skipcba = ConvBNAct(in_ch, out_ch, None, kernel_size=(1, 1), padding=(0, 0))
-
-        self.cba1 = ConvBNAct(in_ch, out_ch, act_fn, (3, 3), padding=(1, 1))
-        self.cba2 = ConvBNAct(out_ch, out_ch, None, (3, 3), padding=(1, 1))
-
-        self.act_fn = act_fn if last_act_fn else None
-        self.cbam = CBAM(out_ch, reduction_ratio=min(16, out_ch)) if use_cbam else None
-
-    def forward(self, x):
-        residual = self.skipcba(x)
-
-        out = self.cba1(x)
-        out = self.cba2(out)
-        if self.cbam:
-            out = self.cbam(out)
-        out += residual
-        if self.act_fn:
-            out = self.act_fn(out)
-
-        return out
-
-
-class FusionNetBlock(nn.Module):
-    def __init__(self, in_ch: int, out_ch: int, act_fn: nn.Module, use_cbam=False):
-        super().__init__()
-        self.cba1 = ConvBNAct(in_ch, out_ch, act_fn, (3, 3), padding=(1, 1))
-        self.resblock = ResidualBlock(out_ch, out_ch, act_fn, last_act_fn=False, use_cbam=use_cbam)
-        self.cba2 = ConvBNAct(out_ch, out_ch, act_fn, (3, 3), padding=(1, 1))
-
-    def forward(self, x):
-        x = self.cba1(x)
-        x = self.resblock(x)
-        x = self.cba2(x)
-        return x
-
-
 class InConv(nn.Module):
     def __init__(self, in_ch: int, out_ch: int, kernel_size=(3, 3)):
         super().__init__()
         padding = (kernel_size[0] // 2, kernel_size[1] // 2)
-        # self.block = FusionNetBlock(in_ch, out_ch, nn.ReLU(inplace=True), use_cbam=use_cbam)
         self.block = nn.Sequential(
             ConvBNAct(in_ch, out_ch, nn.ReLU(inplace=True), kernel_size, padding),
             ConvBNAct(out_ch, out_ch, nn.ReLU(inplace=True), kernel_size, padding),
         )
-        # self.conv = ResidualBlock(in_ch, out_ch)
 
     def forward(self, x):
         x = self.block(x)
@@ -109,10 +63,9 @@ class DownAndConv(nn.Module):
         padding = (kernel_size[0] // 2, kernel_size[1] // 2)
         self.pool = nn.MaxPool2d((2, 2))
 
-        # self.block = FusionNetBlock(in_ch, out_ch, nn.ReLU(inplace=True), use_cbam=use_cbam)
         self.block = nn.Sequential(
-            ConvBNAct(in_ch, out_ch, nn.ReLU(inplace=True), kernel_size, padding, stride=stride),
-            ConvBNAct(out_ch, out_ch, nn.ReLU(inplace=True), kernel_size, padding),
+            ConvBNAct(in_ch, out_ch, nn.LeakyReLU(0.2, True), kernel_size, padding, stride=stride),
+            ConvBNAct(out_ch, out_ch, nn.LeakyReLU(0.2, True), kernel_size, padding),
         )
 
     def forward(self, x):
@@ -130,7 +83,6 @@ class UpAndConv(nn.Module):
         else:
             self.up = nn.ConvTranspose2d(in_ch, out_ch, upsample, stride=upsample)
 
-        # self.block = FusionNetBlock(out_ch, out_ch, nn.ReLU(inplace=True), use_cbam=use_cbam)
         self.block = nn.Sequential(
             ConvBNAct(out_ch, out_ch, nn.ReLU(inplace=True), kernel_size, padding),
             ConvBNAct(out_ch, out_ch, nn.ReLU(inplace=True), kernel_size, padding),
@@ -140,7 +92,6 @@ class UpAndConv(nn.Module):
         x = self.up(x)
         x, x_skip = force_size_same(x, x_skip)
 
-        # x = torch.cat([x_skip, x_decode], dim=1)
         out = (x + x_skip) / 2
         out = self.block(out)
         return out
@@ -149,8 +100,8 @@ class UpAndConv(nn.Module):
 class OutConv(nn.Module):
     def __init__(self, in_ch: int, out_ch: int):
         super().__init__()
-        # self.conv1 = nn.Conv2d(in_ch, out_ch, (3, 3), padding=(1, 1))
-        self.conv1 = nn.Conv2d(in_ch, out_ch, (1, 1))
+        self.conv1 = nn.Conv2d(in_ch, out_ch, (1, 5), padding=(0, 2))
+        # self.conv1 = nn.Conv2d(in_ch, out_ch, (1, 1))
         self.gap = nn.AdaptiveAvgPool2d((1, None))
 
     def forward(self, x):
