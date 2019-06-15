4b3d22d6f22485e0c48dc6ce1e97841471a14a59
Sytronik <sytronik210@gmail.com>
2019-06-15 12:38:26 +0900

    refactor

--- utils.py
+++ utils.py
@@ -10,63 +10,17 @@ from numpy import ndarray
 from torch import Tensor
 
 
-def draw_segmap(song_id: int, segmap: ndarray, sect_names: List[str] = None):
-    """
-
-    :param song_id:
-    :param segmap: (T,)
-    :param sect_names: (C,)
-    :return:
-    """
-    if not hasattr(draw_segmap, 'cmap'):
-        draw_segmap.cmap = ([plt.cm.tab20b(i) for i in range(20)]
-                            + [plt.cm.tab20c(i) for i in range(20)])
-        draw_segmap.cmap = np.array(draw_segmap.cmap)
-
-    colors = draw_segmap.cmap[segmap]
-    x = np.arange(len(segmap))
-    xlim = [-0.5, x[-1] + 0.5]
-
-    if sect_names:
-        fig = plt.figure(figsize=(x[-1] // 1000, 3.5))
-        plt.subplot(2, 1, 1, title=str(song_id))
-        plt.bar(x, height=1, width=1, color=colors)
-        plt.xlim(xlim)
-        plt.xlabel('time frame index')
-        plt.yticks([])
-
-        n_labels = len(sect_names)
-        x_cbar = np.arange(n_labels)
-        xlim_cbar = [-0.5, x_cbar[-1] + 0.5]
-        plt.subplot(2, 1, 2, title='colorbar')
-        plt.bar(x_cbar, height=1, width=1, color=draw_segmap.cmap[:n_labels])
-        plt.xlim(xlim_cbar)
-        plt.xticks(x_cbar, sect_names, rotation='vertical')
-        plt.yticks([])
-    else:
-        fig = plt.figure(figsize=(x[-1] // 1000, 1.5))
-        plt.title(str(song_id))
-        plt.bar(x, height=1, width=1, color=colors)
-        plt.xlim(xlim)
-        plt.yticks([])
-
-    fig.tight_layout()
-    return fig
-
-
 def draw_lineplot(t_axis: ndarray, score: ndarray, prediction: ndarray, truth: ndarray,
                   song_id: int):
-    fig = plt.figure(figsize=(len(score) // 1000, 2))
+    fig, ax = plt.subplots(figsize=(len(score) // 1000, 2))
 
-    plt.plot(t_axis, score, zorder=1)
+    ax.plot(t_axis, score, zorder=1)
 
-    ax = plt.gca()
     ax.vlines(x=prediction, ymin=0.7, ymax=1, colors='r', label='prediction', zorder=2)
     ax.vlines(x=truth, ymin=0, ymax=0.3, colors='y', label='truth', zorder=2)
 
     ax.set_title(str(song_id))
     ax.legend(loc='upper right')
-    # ax.set_xticks(truth)
     ax.set_yticks([0, 0.5, 1])
     ax.grid(True, axis='y')
     ax.set_xlabel('time (sec)')
